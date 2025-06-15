38ddd3049992c67876eb61a193ed2a8eab2cf588
Andarios910 <andarioskarokaro92@gmail.com>
2025-06-15 11:22:35 +0700

    add: new table user likes migrations

---
+++ migrations/1749960829160_create-table-user-comment-likes.js
@@ -0,0 +1,42 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  pgm.createTable("user_comment_likes", {
+    id: {
+      type: "VARCHAR(50)",
+      primaryKey: true,
+    },
+    comment: {
+      type: "VARCHAR(50)",
+      notNull: true,
+      references: "comments",
+    },
+    owner: {
+      type: "VARCHAR(50)",
+      notNull: true,
+      references: "users",
+    },
+  });
+
+  pgm.addConstraint(
+    "user_comment_likes",
+    "unique_comment_and_owner",
+    "UNIQUE(comment, owner)"
+  );
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+  pgm.dropTable("user_comment_likes");
+};
