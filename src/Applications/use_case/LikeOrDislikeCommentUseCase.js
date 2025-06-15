c14f5b1014547a583851a97262be790220d5e743
Andarios910 <andarioskarokaro92@gmail.com>
2025-06-15 11:23:05 +0700

    add: likes repository and use case

---
+++ src/Applications/use_case/LikeOrDislikeCommentUseCase.js
@@ -0,0 +1,29 @@
+const Like = require("../../Domains/likes/entities/Like");
+
+class LikeOrDislikeCommentUseCase {
+  constructor({ commentLikeRepository, commentRepository, threadRepository }) {
+    this._commentLikeRepository = commentLikeRepository;
+    this._commentRepository = commentRepository;
+    this._threadRepository = threadRepository;
+  }
+
+  async execute(userId, useCaseParams) {
+    const { threadId, commentId } = useCaseParams;
+    await this._threadRepository.checkThreadAvailability(threadId);
+    await this._commentRepository.checkCommentAvailability(commentId);
+
+    const like = new Like({
+      commentId,
+      owner: userId,
+    });
+
+    const isCommentLiked =
+      await this._commentLikeRepository.verifyUserCommentLike(like);
+
+    return (await isCommentLiked)
+      ? this._commentLikeRepository.deleteLike(like)
+      : this._commentLikeRepository.addLike(like);
+  }
+}
+
+module.exports = LikeOrDislikeCommentUseCase;
